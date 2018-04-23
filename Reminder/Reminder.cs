1c8f9083131598acc7a1a0164e47d22b9645c36a
Rutwik Choughule <Rutwik.Choughule@xoriant.com>
2018-04-23 14:14:23 +0530

    created Create Reminder Form

---
+++ Reminder/Reminder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reminder
+{
+    public class Reminder
+    {
+        DateTime RemDate { get; set; }
+        String RemTitle { get; set; }
+        Char RemRepeat { get; set; }
+    }
+}
